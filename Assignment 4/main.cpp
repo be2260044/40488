a6d960d7576c63effcde7b8b45d012980656015c
be2260044 <erwin.bradleys@gmail.com>
2017-01-20 10:40:10 -0800

    Signed-off-by: be2260044 <erwin.bradleys@gmail.com>

--- Assignment 4/main.cpp
+++ Assignment 4/main.cpp
@@ -24,48 +24,138 @@ using namespace std;
 int main(int argc, char** argv) {
     //Declare Variables
     char choice;
-    
+    char back;
     //Loop on the menu
     do{
     
         //Input values
+        cout<<"All problems are from Gaddis, 7th Edition, chapter 5 "
+                "and the problem # is in () in the menu"<<endl;
         cout<<"Choose from the list"<<endl;
-        cout<<"Type 1 for Problem with Do-While"<<endl;
-        cout<<"Type 2 for Problem with While"<<endl;
-        cout<<"Type 3 for Problem with For"<<endl;
-        cout<<"Type 4 for Problem with Do-While"<<endl;
-        cout<<"Type 5 for Problem with While"<<endl;
-        cout<<"Type 6 for Problem with For"<<endl;
-        cout<<"Type 7 for Penny Doubling Program"<<endl;
-        cout<<"Type 8 for Celsius to Fahrenheit Table"<<endl;
-        cout<<"Type 9 for Random Guess Game"<<endl;
+        cout<<"Type 1 for Sum of Numbers (1)"<<endl;
+        cout<<"Type 2 for ASCII Code (2)"<<endl;
+        cout<<"Type 3 for Ocean Levels (3)"<<endl;
+        cout<<"Type 4 for Calories Burned (4)"<<endl;
+        cout<<"Type 5 for Membership Fees (5)"<<endl;
+        cout<<"Type 6 for Distance Traveled (6)"<<endl;
+        cout<<"Type 7 for Penny Doubling Program (7)"<<endl;
+        cout<<"Type 8 for Celsius to Fahrenheit Table (12)"<<endl;
+        cout<<"Type 9 for Random Guess Game (20)"<<endl;
+        cout<<"Type 0 to Exit The Program"<<endl;
         cin>>choice;
 
         //Switch to determine the Problem
         switch(choice){
+//Problem 1 Sum of Numbers
             case '1':{
-                
-                break;
+                unsigned int numSum, sum=0;
+
+                //Input or initialize values Here
+                cout<<"This program sums the number "
+                        "from 1 to whatever number you choose."<<endl;
+                cout<<"What number do you want the sum to?"<<endl;
+                cin>>numSum;
+                //validate input
+                while (numSum<1){
+                    cout<<"That is not a valid input, please enter "
+                    "a number greater than 0"<<endl;
+                    cin>>numSum;
+                    }
+                //start loop
+                for(int numb=1;numb<=numSum;numb++){
+                    sum+=numb;
+
+                };
+                cout<<"Sum of integers from 1 to "<<numSum<<" = "<<sum<<endl;
+                break; 
             }
+//Problem 2 ASCII Code          
             case '2':{
-                
+                for(int ASCII=1;ASCII<=127;ASCII++){
+                if(ASCII%16==0)//makes 16 ASCII characters per line
+                cout<<endl;
+        
+                 cout<<static_cast<char>(ASCII)<<"  "; 
+        
+                 };
                 break;
             }
+//Problem 3 Ocean Heights           
             case '3':{
-                
+                float ocnHght;//ocean height
+                //Input or initialize values Here
+                cout<<"This table shows how much the ocean "
+                             "will rise over the next 25 years:"<<endl;
+                for(int year=1;year<=25;year++){
+                    ocnHght=year*1.5;//add 1.5 mm for every year
+
+
+                    cout<<"Year "<<year<<" = "<<ocnHght<<" millimeters"<<endl; 
+
+                };
+                cout<<"The oceans will rise "<<ocnHght<<
+                        " millimeters by the end of 25 years from now."<<endl;
             }
+//Problem 4 Calories Burned            
             case '4':{
-                
+                //Declare and initialize local Variables Here
+                float clsBrnd;//calories burned
+
+                //Input or initialize values Here
+                cout<<"This table shows how many calories are burned "
+                             "while on the treadmill over time"<<endl;
+                for(int minsRan=2;minsRan<=6;minsRan++){//start at 2 to start 10
+                    clsBrnd=minsRan*5*3.9;//3.9 calories for every minute ran
+                                          //5 to get results in increments of 5
+                    cout<<"After "<<minsRan*5<<" minutes ran, you burned "
+                            <<clsBrnd<<" calories total"<<endl; 
+       
+                }
                 break;
             }
+//Problem 5 Membership Fees            
             case '5':{
-                
+                //Declare and initialize local Variables Here
+                float memFees=2500;//membership fees
+
+                //Input or initialize values Here
+                cout<<"This table shows how much the membership fees "
+                             "of a club increases over the next 6 years"<<endl;
+                for(int year=1;year<=6;year++){
+                    memFees*=1.04;//2500 starting, increasing by 4% each year
+                    cout<<setprecision(2)<<fixed;
+                    cout<<"After year "<<year<<", the membership fees will be $"
+                            <<memFees<<endl; 
+
+                }
                 break;
             }
+//Problem 6 Distance Traveled            
             case '6':{
-                
+                 //Declare and initialize local Variables Here
+                int     tmeTrvd;//time traveled in hours
+
+                float   mlsPrHr,//miles per hour 
+                       dstTrvd;//distance traveled in miles
+                //Input or initialize values Here
+                cout<<"This program calculates how much distance a vehicle "
+                             "travels over a certain amount of time."<<endl;
+                cout<<"How fast was the vehicle moving?"
+                        " (in miles per hour, and must be positive"<<endl;
+                cin>>mlsPrHr;
+                cout<<"How many hours did it travel?"<<endl;
+                cin>>tmeTrvd;
+                //start loop
+                for(int time=1;time<=tmeTrvd;time++){
+                    dstTrvd=mlsPrHr*time;//distance=speed*time
+                    cout<<setprecision(2)<<fixed;
+                    cout<<"After hour #"<<time<<", the vehicle traveled "
+                            <<dstTrvd<<" miles."<<endl; 
+
+                }
+                break;
             }
-//Penny Doubling
+//Problem 7 Penny Doubling
             case '7':{
                 int salary=1,totPay=0,maxDays;//Salary and Pay in pennies
     
@@ -98,7 +188,7 @@ int main(int argc, char** argv) {
                 
                 break;
             }
-//Celsius to Fahrenheit             
+//Problem 12 Celsius to Fahrenheit             
             case '8':{
             //Declare and initialize local Variables Here
             float fhtTemp;//Celsius temp and Fahrenheit temp
@@ -116,7 +206,7 @@ int main(int argc, char** argv) {
                 }
                 break;
             }
-//Random Number Guessing Game:            
+//Problem 20 Random Number Guessing Game           
             case '9':{
                 //set random number seed
             srand(static_cast<unsigned int>(time(0)));
--- Homework/Assignment 4/Gaddis_7thEd_Chap5_Prob1_SumOfNumbers/main.cpp
+++ Homework/Assignment 4/Gaddis_7thEd_Chap5_Prob1_SumOfNumbers/main.cpp
@@ -20,7 +20,7 @@ using namespace std;
 int main(int argc, char** argv) {
  
     //Declare and initialize local Variables Here
-    unsigned int numSum, sum;
+    unsigned int numSum, sum=0;
    
     
     
@@ -30,13 +30,18 @@ int main(int argc, char** argv) {
     cout<<"What number do you want the sum to? (MUST BE A POSITIVE INTEGER)"
             <<endl;
     cin>>numSum;
+    while (numSum<1){
+        cout<<"That is not a valid input, please enter "
+                "a number greater than 0"<<endl;
+        cin>>numSum;
+    }
     for(int numb=1;numb<=numSum;numb++){
         //assign a grade and add to the average
         //score and grade are local to the while group
         sum+=numb;
         
         //Process/Calculations Here
-        
+        cout<<numb<<"     "<<sum<<endl;
     
         //Output Located Here
          
