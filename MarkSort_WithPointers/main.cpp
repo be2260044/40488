36060a93dce499935dcbac5a2aa8a1ddc9b1b7ca
rcc <rcc>
2017-02-07 11:37:44 -0800

    Signed-off-by: rcc <rcc>

--- MarkSort_WithPointers/main.cpp
+++ MarkSort_WithPointers/main.cpp
@@ -17,11 +17,11 @@ using namespace std;
 //Like PI, e, Gravity, or conversions
 
 //Function Prototypes Here
-void filAray(int *,int);
-void prntAry(int *,int,int);
-void swap(int *,int *);
-void smalst(int *,int,int);
-void markSrt(int *,int);
+void filAray(int [],int);
+void prntAry(int [],int,int);
+void swap(int &,int &);
+void smalst(int [],int,int);
+void markSrt(int [],int);
 //Program Execution Begins Here
 int main(int argc, char** argv) {
     //Set rnd numb seed
@@ -44,7 +44,7 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-void markSrt(int *a,int n){
+void markSrt(int a[],int n){
     for(int i=0;i<n-1;i++){
         smalst(a,n,i);
     }
@@ -52,14 +52,14 @@ void markSrt(int *a,int n){
 
 void smalst(int a[],int n,int pos){
     for(int i=pos+1;i<n;i++){
-        if(a[pos]>a[i])swap(*(a+pos),a[i]);
+        if(a[pos]>a[i])swap(a[pos],a[i]);
     }
 }
 
-void swap(int *a,int *b){
-    *a=*a^*b;
-    *b=*a^*b;
-    *a=*a^*b;
+void swap(int &a,int &b){
+    a=a^b;
+    b=a^b;
+    a=a^b;
 }
 
 void prntAry(int a[],int n,int perLine){
