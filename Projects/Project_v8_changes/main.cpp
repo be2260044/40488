abcded618c24aa6acd1b492d2aed64f5f38e56d8
rcc <rcc>
2017-02-08 11:32:57 -0800

    Signed-off-by: rcc <rcc>

--- Projects/Project_v8_changes/main.cpp
+++ Projects/Project_v8_changes/main.cpp
@@ -137,125 +137,125 @@ char gtChce(){
 
 void gamePly(long int &potAmnt,long int &cMoney,int cNum){
     for(int game=0;(game<=2)&&(cMoney>0)&&(potAmnt>0);game++){
-                                     //Call random number generator for the dice
-            char die1=rand()%12+1;   //Value from 1-12; Sets a Limit
-            char die2=rand()%12+1;   //Value from 1-12; Sets a Limit
-            char die3=rand()%12+1;   //Value from 1-12
-            int betAmt3=rand()%100+1;//Betting Amount from 1-100
+                                 //Call random number generator for the dice
+        char die1=rand()%12+1;   //Value from 1-12; Sets a Limit
+        char die2=rand()%12+1;   //Value from 1-12; Sets a Limit
+        char die3=rand()%12+1;   //Value from 1-12
+        int betAmt3=rand()%100+1;//Betting Amount from 1-100
 
-            //Validate to not have same limits
-            while(die1==die2){
-                die2=rand()%12+1;//Reroll second die
-            }
+        //Validate to not have same limits
+        while(die1==die2){
+            die2=rand()%12+1;//Reroll second die
+        }
 
-            //Display the 2 limits
-            cout<<static_cast<int>(die1)<<"    "<<static_cast<int>(die2)<<endl; 
-            cout<<endl;
-            while(potAmnt<betAmt3){
-                betAmt3/=2;
-            }
-            cout<<"Computer "<<cNum<<" bets "<<"$"<<betAmt3<<endl;
-            cout<<endl;
-            cout<<"The next Roll is:"<<static_cast<int>(die3)<<endl;
-            cout<<endl;
-            (die3>die1&&die3<die2)||(die3<die1&&die3>die2)?//Win?
-                    (cMoney+=betAmt3)&&      //C1's money increases
-                        (potAmnt-=betAmt3)&&  //Money pot decreases
-                        (cout<<"Computer "<<cNum<<" Won $"<<betAmt3<<endl)://Lose?
-                (die3==die1||die3==die2)?    //Did the roll land on a limit?
-                    (cMoney-=betAmt3*2)&&    //If true, lose double the bet
-                        (potAmnt+=betAmt3*2)&&//Money pot increases
-                        (cout<<"Computer "<<cNum<<" Lost $"<<2*betAmt3<<endl):
-                    (cMoney-=betAmt3)&&     //Regular loss, just lose bet
-                        (potAmnt+=betAmt3)&& //Money pot increases
-                        (cout<<"Computer "<<cNum<<" Lost $"<<betAmt3<<endl);
-            cout<<"Computer "<<cNum<<" has $"<<cMoney<<" left."<<endl;
-            cout<<"There is $"<<potAmnt<<" left in the pot."<<endl;
-            cout<<endl;
-            (potAmnt<1)?(cout<<"No money left "
-                    "in the pot!"<<endl)&&(game+=3):' ';
-            (cMoney<1)?(cout<<"Computer "<<cNum<<" is out"
-                    " of money!"<<endl)&&(game+=3):' ';
-            }
+        //Display the 2 limits
+        cout<<static_cast<int>(die1)<<"    "<<static_cast<int>(die2)<<endl; 
+        cout<<endl;
+        while(potAmnt<betAmt3){
+            betAmt3/=2;
+        }
+        cout<<"Computer "<<cNum<<" bets "<<"$"<<betAmt3<<endl;
+        cout<<endl;
+        cout<<"The next Roll is:"<<static_cast<int>(die3)<<endl;
+        cout<<endl;
+        (die3>die1&&die3<die2)||(die3<die1&&die3>die2)?//Win?
+                (cMoney+=betAmt3)&&      //C1's money increases
+                    (potAmnt-=betAmt3)&&  //Money pot decreases
+                    (cout<<"Computer "<<cNum<<" Won $"<<betAmt3<<endl)://Lose?
+            (die3==die1||die3==die2)?    //Did the roll land on a limit?
+                (cMoney-=betAmt3*2)&&    //If true, lose double the bet
+                    (potAmnt+=betAmt3*2)&&//Money pot increases
+                    (cout<<"Computer "<<cNum<<" Lost $"<<2*betAmt3<<endl):
+                (cMoney-=betAmt3)&&     //Regular loss, just lose bet
+                    (potAmnt+=betAmt3)&& //Money pot increases
+                    (cout<<"Computer "<<cNum<<" Lost $"<<betAmt3<<endl);
+        cout<<"Computer "<<cNum<<" has $"<<cMoney<<" left."<<endl;
+        cout<<"There is $"<<potAmnt<<" left in the pot."<<endl;
+        cout<<endl;
+        (potAmnt<1)?(cout<<"No money left "
+                "in the pot!"<<endl)&&(game+=3):' ';
+        (cMoney<1)?(cout<<"Computer "<<cNum<<" is out"
+                " of money!"<<endl)&&(game+=3):' ';
+        }
 }
 
 void gamePly(long int &potAmnt,long int &monyLft,unsigned short &wins,
         unsigned short &losses,unsigned short &nGames){
     //First Loop is for the player
-        for(int game=0;(game<=2)&&(monyLft>0)&&(potAmnt>0);game++){
-            //Call random number generator for the dice
-            char die1=rand()%12+1;//Value from 1-12; Sets a Limit
-            char die2=rand()%12+1;//Value from 1-12; Sets a Limit
-            char die3=rand()%12+1;//Value from 1-12
-            char decPlay;//Decision to Play?
-            int betAmnt=0;//Betting Amount
-            bool test1=(die3>die1&&die3<die2)||
-                        (die3<die1&&die3>die2),//Did roll land within limits?
-                    test2=(die3==die1||die3==die2);//Did roll land on a limit?
+    for(int game=0;(game<=2)&&(monyLft>0)&&(potAmnt>0);game++){
+        //Call random number generator for the dice
+        char die1=rand()%12+1;//Value from 1-12; Sets a Limit
+        char die2=rand()%12+1;//Value from 1-12; Sets a Limit
+        char die3=rand()%12+1;//Value from 1-12
+        char decPlay;//Decision to Play?
+        int betAmnt=0;//Betting Amount
+        bool test1=(die3>die1&&die3<die2)||
+                    (die3<die1&&die3>die2),//Did roll land within limits?
+                test2=(die3==die1||die3==die2);//Did roll land on a limit?
 
-            //Validate to not have same limits
-            while(die1==die2){
-                die2=rand()%12+1;//Reroll second die
-            }
+        //Validate to not have same limits
+        while(die1==die2){
+            die2=rand()%12+1;//Reroll second die
+        }
 
-            //Show how much money is in player's pocket and in the pot
-            cout<<"You have $"<<monyLft<<" left."<<endl;
-            cout<<"There is $"<<potAmnt<<" left in the pot."<<endl;
-            
-            //Display the 2 limits
-            cout<<static_cast<int>(die1)<<"    "<<static_cast<int>(die2)<<endl;
-            cout<<"Press 1 to Play or 2 to Pass"<<endl;
+        //Show how much money is in player's pocket and in the pot
+        cout<<"You have $"<<monyLft<<" left."<<endl;
+        cout<<"There is $"<<potAmnt<<" left in the pot."<<endl;
+
+        //Display the 2 limits
+        cout<<static_cast<int>(die1)<<"    "<<static_cast<int>(die2)<<endl;
+        cout<<"Press 1 to Play or 2 to Pass"<<endl;
+        cin>>decPlay;
+        cout<<endl;
+        while(decPlay<'1'||decPlay>'2'){            //Validate play decision
+            cout<<"That is not a valid input, please enter 1 to play "
+                    "or 2 to pass"<<endl;
             cin>>decPlay;
             cout<<endl;
-            while(decPlay<'1'||decPlay>'2'){            //Validate play decision
-                cout<<"That is not a valid input, please enter 1 to play "
-                        "or 2 to pass"<<endl;
-                cin>>decPlay;
-                cout<<endl;
+        }
+        if(decPlay=='2');                      //If pass, do nothing
+        else if(decPlay=='1'){                 //Execute play
+            cout<<"How much would you like to bet?"<<endl;
+            cin>>betAmnt;                      //Enter bet
+            while(potAmnt<betAmnt){
+                cout<<"Not enough funds in the pot, "
+                        "please enter an amount that "
+                        "is a max of $"<<potAmnt<<endl;
+                cin>>betAmnt;
             }
-            if(decPlay=='2');                      //If pass, do nothing
-            else if(decPlay=='1'){                 //Execute play
-                cout<<"How much would you like to bet?"<<endl;
-                cin>>betAmnt;                      //Enter bet
-                while(potAmnt<betAmnt){
-                    cout<<"Not enough funds in the pot, "
-                            "please enter an amount that "
-                            "is a max of $"<<potAmnt<<endl;
-                    cin>>betAmnt;
-                }
-                cout<<endl;
-                cout<<"The next Roll is:"<<static_cast<int>(die3)<<endl;
-                cout<<endl;
-                if(test1){
-                    monyLft+=betAmnt;//Your money increases
-                    wins++;
-                    potAmnt-=betAmnt;//Money pot decreases                           
-                    cout<<"You Won $"<<betAmnt<<endl;
-                }
-                else if(test2){
-                    monyLft-=betAmnt*2;//If true, lose double the bet
-                    potAmnt+=betAmnt*2;//Money pot increases
-                    cout<<"You Lost $"<<2*betAmnt<<endl;
-                    losses++;
-                }
-                else if(!test2){   
-                    monyLft-=betAmnt;//Regular loss, just lose bet
-                    potAmnt+=betAmnt;//Money pot increases
-                    cout<<"You Lost $"<<betAmnt<<endl;
-                    losses++;
-                }
-                else cout<<"Error"<<endl;
-                cout<<endl;
-                nGames++;                       //Increment Games Played
-                }
-            if (potAmnt<1){
-                    cout<<"No money left in the pot!"<<endl;
-                    game+=3;
-                }
-                if (monyLft<1){
-                    cout<<"You are out of money!"<<endl;
-                    game+=3;
-                }
-                
-        } 
+            cout<<endl;
+            cout<<"The next Roll is:"<<static_cast<int>(die3)<<endl;
+            cout<<endl;
+            if(test1){
+                monyLft+=betAmnt;//Your money increases
+                wins++;
+                potAmnt-=betAmnt;//Money pot decreases                           
+                cout<<"You Won $"<<betAmnt<<endl;
+            }
+            else if(test2){
+                monyLft-=betAmnt*2;//If true, lose double the bet
+                potAmnt+=betAmnt*2;//Money pot increases
+                cout<<"You Lost $"<<2*betAmnt<<endl;
+                losses++;
+            }
+            else if(!test2){   
+                monyLft-=betAmnt;//Regular loss, just lose bet
+                potAmnt+=betAmnt;//Money pot increases
+                cout<<"You Lost $"<<betAmnt<<endl;
+                losses++;
+            }
+            else cout<<"Error"<<endl;
+            cout<<endl;
+            nGames++;                       //Increment Games Played
+            }
+        if (potAmnt<1){
+                cout<<"No money left in the pot!"<<endl;
+                game+=3;
+            }
+            if (monyLft<1){
+                cout<<"You are out of money!"<<endl;
+                game+=3;
+            }
+
+    } 
 }
